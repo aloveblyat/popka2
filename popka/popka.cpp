dca7d1a7ab2f86ad120d5eb65bc09bc3e278d99b
kbstudent <kbstudent@KB37-309-C13>
2024-09-10 11:21:04 +0300

    Добавьте файлы проекта.

---
+++ popka/popka.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string> 
+using namespace std;
+
+int main()
+{
+    int int_1 = 20;
+    string s1 = "hello"; 
+    char ch1 = 's'; 
+    double d1 = 25;
+    float f1 = 25.6f; 
+    int a1 = 25; 
+    char ch2 = 'a';
+    char a2 = 'H'; 
+    string str = "Hello"; 
+    char b1 = '8';
+    int a3; 
+    int c3 = 18;
+    s1 = "hi"; 
+    const char* arr = "lets go"; 
+
+    return 0; 
+}
